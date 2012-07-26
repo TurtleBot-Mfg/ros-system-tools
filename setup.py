4466aba792a5defae8a3094e9398efa2e6c95711
I Heart Engineering <code@iheartengineering.com>
2012-07-26 19:41:38 -0400

    Initial commit!

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+import os
+import subprocess
+
+"""
+if os.path.exists("debian/changelog"):
+    output=subprocess.check_output("parsechangelog | grep Version", shell=True)
+    version = output.split(":")[1].strip()
+"""
+
+setup(name = "ros-system-tools",
+#    version = version,
+    version = "0.0.1",
+    description = "System level ROS Tools",
+    author = "I Heart Engineering",
+    author_email = "code@iheartengineering.com",
+    url = "http://www.iheartengineering.com",
+    license = "BSD-3-clause",
+    scripts = ["ros-network-id"],
+    long_description = """These tools provide system support for ROS.""" 
+    #classifiers = []     
+) 
